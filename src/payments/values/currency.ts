1fbed499430973a37d934770e4a0372003dc71c1
Radek Kyselý <radek@kysely.org>
2025-10-24 00:33:34 +0200

    feat(payments): Restrict Currency tokens

--- src/payments/values/currency.ts
+++ src/payments/values/currency.ts
@@ -1,5 +1,5 @@
 import { z } from "zod";
-import { TOKENS } from "./token.js";
+import { STABLECOIN_TOKENS, NATIVE_TOKENS } from "./token.js";
 
-export const Currency = z.enum(TOKENS);
+export const Currency = z.enum([...STABLECOIN_TOKENS, ...NATIVE_TOKENS]);
 export type Currency = z.infer<typeof Currency>;
