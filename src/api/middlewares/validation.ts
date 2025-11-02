da031918d6e797d4e64bb6e9501f86dc0d5c9bff
Radek Kyselý <radek@kysely.org>
2025-11-02 14:01:17 +0100

    chore(api): Allow custom validation error formatting

--- src/api/middlewares/validation.ts
+++ src/api/middlewares/validation.ts
@@ -1,25 +1,47 @@
-import { ValidationTargets } from "hono";
+import { Context, ValidationTargets } from "hono";
 import { zValidator } from "@hono/zod-validator";
 import { ProblemJson } from "@/api/values";
 
+export type ApiValidationError = {
+  path: (string | number | symbol)[];
+  message: string;
+};
+
+export type CreateValidationErrorResponse = (
+  c: Context,
+  target: Parameters<typeof zValidator>[0],
+  errors: ApiValidationError[]
+) => Response;
+
 export const withValidation = (
   target: Parameters<typeof zValidator>[0],
-  schema: Parameters<typeof zValidator>[1]
+  schema: Parameters<typeof zValidator>[1],
+  createErrorResponse: CreateValidationErrorResponse = createValidationErrorResponse
 ) =>
   zValidator(target, schema, (result, c) => {
     if (!result.success) {
-      const errors = result.error.issues.map(
-        (error) => `[${error.path.join("/")}] ${error.message}`
-      );
+      const errors: ApiValidationError[] = result.error.issues.map((error) => ({
+        path: error.path,
+        message: error.message,
+      }));
 
-      return ProblemJson(
-        c,
-        400,
-        "Bad request",
-        `Invalid ${PLACEMENT[target]} shape: ${errors}`
-      );
+      return createErrorResponse(c, target, errors);
     }
   });
+
+const createValidationErrorResponse: CreateValidationErrorResponse = (
+  c,
+  target,
+  errors
+) => {
+  const serializedErrors = errors
+    .map((error) => `[${error.path.join("/")}] ${error.message}`)
+    .join(", ");
+
+  const errorMessage = `Invalid ${PLACEMENT[target]} shape: ${serializedErrors}`;
+
+  return ProblemJson(c, 400, "Bad request", errorMessage);
+};
 
 const PLACEMENT: { [key in keyof ValidationTargets]: string } = {
   json: "body",
