4dd5c730763f6a068f4a1b42754d5a13db83b11a
Radek Kyselý <radek@kysely.org>
2025-11-09 00:50:28 +0100

    feat(web): Add meta tags to head

--- src/web/components/Layout.tsx
+++ src/web/components/Layout.tsx
@@ -4,8 +4,18 @@ export const Layout: FC = (props) => {
   return (
     <html lang="en">
       <head>
-        <meta charset="UTF-8" />
+        <meta charSet="UTF-8" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta
+          name="description"
+          content="Arc Pay: Secure and easy way for AI agents to pay for goods and services autonomously."
+        />
+        <meta
+          name="keywords"
+          content="Arc Pay, payments, AI agents, USDC, Arc Network"
+        />
+        <link rel="icon" href="/favicon.ico" />
         <title>Arc Pay</title>
         <link rel="stylesheet" href="/main.css" />
       </head>
