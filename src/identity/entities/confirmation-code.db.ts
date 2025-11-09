668ea394bc36a7f52e24899b723075c291ecf7e8
Radek Kyselý <radek@kysely.org>
2025-11-09 01:14:17 +0100

    feat(identity): Longer confirmation code expiration time

--- src/identity/entities/confirmation-code.db.ts
+++ src/identity/entities/confirmation-code.db.ts
@@ -1,19 +1,27 @@
 import { db } from "@/database";
 import { ConfirmationCode } from "@/identity/entities/confirmation-code.entity";
+import { MINUTE } from "@/lib";
 
-const minute = 60 * 1000;
+const EMAIL_CODE_EXPIRATION_TIME = 2 * MINUTE;
+const PHONE_CODE_EXPIRATION_TIME = 1 * MINUTE;
 
 const storageKey = ({ code }: { code: number }) => `conf_code:${code}`;
 
 export const saveConfirmationCode = async (
   confirmationCode: ConfirmationCode
 ) => {
+  const expirationTime = confirmationCode.phone
+    ? PHONE_CODE_EXPIRATION_TIME
+    : EMAIL_CODE_EXPIRATION_TIME;
+
   await db
     .multi()
     .set(storageKey({ code: confirmationCode.code }), confirmationCode)
     .expireat(
       storageKey({ code: confirmationCode.code }),
-      Math.floor((confirmationCode.created_at.getTime() + minute) / 1000)
+      Math.floor(
+        (confirmationCode.created_at.getTime() + expirationTime) / 1000
+      )
     )
     .exec();
   return confirmationCode;
