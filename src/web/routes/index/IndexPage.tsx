2d549493885ee8402778964539614df9d9487f7c
Radek Kyselý <radek@kysely.org>
2025-11-25 21:06:41 +0100

    feat(web): Tweaked copy

--- src/web/routes/index/IndexPage.tsx
+++ src/web/routes/index/IndexPage.tsx
@@ -15,20 +15,37 @@ export const IndexPage: FC<IndexPageProps> = (props: IndexPageProps) => {
       <div className="index">
         <div className="content">
           <h1 class="fade-in">
-            Enable payments
+            Safe payments
             <br />
             for your AI agents
           </h1>
 
+          {/* <p class="fade-in">
+            Arc Pay lets your AI assistants purchase&nbsp;physical & digital
+            resources autonomously—within the boundaries you&nbsp;define.
+          </p> */}
+
+          {/* <p class="fade-in">
+            Allow your AI agents to make approved purchases within your spending
+            caps, permissions, and audit trails. No surprise charges.
+          </p> */}
+
+          {/* <p class="fade-in">
+            Arc Pay lets your AI agents make approved purchases on their own.
+            You set the limits and rules, and every payment stays visible and
+            controlled.
+          </p> */}
+
           <p class="fade-in">
-            Arc Pay is secure and easy way for your AI agents to pay for
-            physical or digital goods and services—autonomously, on your terms
+            Arc Pay lets your AI assistants make approved purchases on their own.
+            You set the limits and rules—and every payment stays visible and
+            under control.
           </p>
 
           <form method="post" action="/login" class="fade-in">
             <div class="form-field">
               <input
-                class="large"
+                class="large highlighted"
                 type="email"
                 id="email"
                 name="email"
@@ -38,7 +55,7 @@ export const IndexPage: FC<IndexPageProps> = (props: IndexPageProps) => {
             </div>
 
             <button type="submit" class="primary large">
-              Get agentic wallet
+              Create agentic wallet
             </button>
           </form>
 
@@ -47,6 +64,16 @@ export const IndexPage: FC<IndexPageProps> = (props: IndexPageProps) => {
             src="/arc_pay_agentic_wallet.png"
             alt="Arc Pay: Agentic wallet"
           />
+
+          <h2 class="cta fade-in">
+            Add payments to
+            <br />
+            your AI workflow
+          </h2>
+
+          <a href="/login" class="button secondary large fade-in">
+            Create agentic wallet
+          </a>
         </div>
       </div>
 
