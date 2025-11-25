c529e37a05ac4b4def5d37d5cab8cdea7dab16ac
Radek Kyselý <radek@kysely.org>
2025-11-25 10:37:49 +0100

    feat(web): Logo small variant w/ link

--- src/web/components/Logo.tsx
+++ src/web/components/Logo.tsx
@@ -2,10 +2,12 @@ import { FC } from "hono/jsx";
 
 type LogoProps = {
   variant: "full" | "short";
-  size: "medium" | "large";
+  size: "small" | "medium" | "large";
+  href?: string;
 };
 
 const logoSizeClass: Record<LogoProps["size"], string> = {
+  small: "logo small",
   medium: "logo medium",
   large: "logo large",
 };
@@ -13,6 +15,7 @@ const logoSizeClass: Record<LogoProps["size"], string> = {
 export const Logo: FC<LogoProps> = (props: LogoProps) => {
   return (
     <div className={logoSizeClass[props.size]}>
+      {props.href && <a href={props.href} />}
       <span>◠</span> {props.variant === "full" ? "Arc Pay" : "Pay"}
     </div>
   );
