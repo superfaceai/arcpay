6476ef35214dc94e60cadda4498936b686e5c8e7
Radek Kyselý <radek@kysely.org>
2025-12-06 03:41:49 +0100

    feat(web): Add Snippet component

---
+++ src/web/components/Snippet.tsx
@@ -0,0 +1,56 @@
+import { FC } from "hono/jsx";
+import { IconCheck, IconCopy, IconEye, IconEyeOff } from "./icons";
+
+interface SnippetProps {
+  content: string;
+  obfuscatedContent?: string;
+}
+
+export const Snippet: FC<SnippetProps> = (props: SnippetProps) => {
+  const snippetId = Math.random().toString(36).slice(2);
+
+  return (
+    <>
+      <div className="snippet">
+        <div className="content public">
+          {props.obfuscatedContent || props.content}
+        </div>
+        {props.obfuscatedContent && (
+          <div className="content obfuscated">{props.content}</div>
+        )}
+
+        <div className="actions">
+          {props.obfuscatedContent && (
+            <button class="ghost show-obfuscated" tabIndex={0}>
+              <input type="checkbox" checked={false} />
+              <span class="icon icon-base">
+                <IconEye />
+              </span>
+              <span class="icon icon-engaged">
+                <IconEyeOff />
+              </span>
+            </button>
+          )}
+
+          <button class="ghost" tabIndex={0} id={snippetId}>
+            <span class="icon icon-base">
+              <IconCopy />
+            </span>
+            <span class="icon icon-engaged">
+              <IconCheck />
+            </span>
+          </button>
+        </div>
+      </div>
+      <script
+        dangerouslySetInnerHTML={{
+          __html: `
+document.getElementById('${snippetId}').addEventListener('click', () => {
+  navigator.clipboard.writeText(\`${props.content}\`);
+});
+      `,
+        }}
+      />
+    </>
+  );
+};
--- src/web/components/icons.tsx
+++ src/web/components/icons.tsx
@@ -77,3 +77,62 @@ export const IconCheck = () => {
     </svg>
   );
 };
+
+export const IconCopy = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    >
+      <rect width="14" height="14" x="8" y="8" rx="2" ry="2" />
+      <path d="M4 16c-1.1 0-2-.9-2-2V4c0-1.1.9-2 2-2h10c1.1 0 2 .9 2 2" />
+    </svg>
+  );
+};
+
+export const IconEye = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    >
+      <path d="M2.062 12.348a1 1 0 0 1 0-.696 10.75 10.75 0 0 1 19.876 0 1 1 0 0 1 0 .696 10.75 10.75 0 0 1-19.876 0" />
+      <circle cx="12" cy="12" r="3" />
+    </svg>
+  );
+};
+
+export const IconEyeOff = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    >
+      <path d="M10.733 5.076a10.744 10.744 0 0 1 11.205 6.575 1 1 0 0 1 0 .696 10.747 10.747 0 0 1-1.444 2.49" />
+      <path d="M14.084 14.158a3 3 0 0 1-4.242-4.242" />
+      <path d="M17.479 17.499a10.75 10.75 0 0 1-15.417-5.151 1 1 0 0 1 0-.696 10.75 10.75 0 0 1 4.446-5.143" />
+      <path d="m2 2 20 20" />
+    </svg>
+  );
+};
